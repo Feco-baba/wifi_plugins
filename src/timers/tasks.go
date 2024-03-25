0946b5c1c6a03b510c6385547d8d70098558a258
Feco <260546078@qq.com>
2024-03-25 18:20:58 +0800

    change the tasks method

--- src/timers/tasks.go
+++ src/timers/tasks.go
@@ -15,24 +15,21 @@ func SSHConnectionCheckingTask() {
 
 func RefreshServerConfigTask() {
 	// 计算每日4点的时刻（假设是UTC时间）
-	now := time.Now().UTC()
-	dailyTime := time.Date(now.Year(), now.Month(), now.Day()+1, 4, 0, 0, 0, time.UTC)
-
+	now := time.Now().UTC().Add(time.Hour * 8)
+	dailyTime := time.Date(now.Year(), now.Month(), now.Day(), 4, 0, 0, 0, time.UTC)
 	// 每次执行完后，重新计算下一天的4点
 	for {
-		// 阻塞主线程，避免频繁计算时间差
-		time.Sleep(15 * time.Minute)
 		// 计算等待时间
 		waitDuration := dailyTime.Sub(now)
-		if waitDuration > 0 {
-			// 使用AfterFunc安排任务在指定时间执行
+		if waitDuration.Hours() > 0 {
 			time.AfterFunc(waitDuration, func() {
 				features.RefreshServerConfig()
 				// 更新下一次执行时间
-				now = time.Now().UTC()
+				now = time.Now().UTC().Add(time.Hour * 8)
 				dailyTime = time.Date(now.Year(), now.Month(), now.Day()+1, 4, 0, 0, 0, time.UTC)
+
 			})
 		}
-
+		time.Sleep(15 * time.Minute)
 	}
 }
